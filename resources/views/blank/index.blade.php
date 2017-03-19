23c025b039853721145974426fbdc1172e5d6445
Marcus Vinícius Campos <marcus_vinicius_campos@hotmail.com>
2017-03-19 16:03:35 -0300

    Templating 100%

--- resources/views/blank/index.blade.php
+++ resources/views/blank/index.blade.php
@@ -1,4 +1,15 @@
-@extends('vendor.template.layout')
-@extends('vendor.template.assets.css')
-@extends('vendor.template.assets.js')
-@extends('vendor.template.menu.left-menu')
+@extends('template')
+
+@section('container')
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="card-box">
+                <h4 class="text-dark header-title m-t-0 m-b-30">Responsive embed video 4:3</h4>
+                <!-- 4:3 aspect ratio -->
+                <div class="embed-responsive embed-responsive-4by3">
+                    <iframe class="embed-responsive-item" src="http://player.vimeo.com/video/69988283?color=5fbeaa&amp;title=0&amp;byline=0&amp;portrait=0"></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/vendor/template/menu/right-menu.blade.php
@@ -0,0 +1,79 @@
+@section('right-menu')
+    <div class="side-bar right-bar nicescroll">
+        <h4 class="text-center">Aulas</h4>
+        <div class="contact-list nicescroll">
+            <ul class="list-group contacts-list">
+                <li class="list-group-item">
+                    <a href="#">
+                        <span class="name">Aula 01</span>
+                        <i class="fa fa-circle online"></i>
+                    </a>
+                    <span class="clearfix"></span>
+                </li>
+                <li class="list-group-item">
+                    <a href="#">
+                        <span class="name">Aula 02</span>
+                        <i class="fa fa-circle online"></i>
+                    </a>
+                    <span class="clearfix"></span>
+                </li>
+                <li class="list-group-item">
+                    <a href="#">
+                        <span class="name">Aula 03</span>
+                        <i class="fa fa-circle online"></i>
+                    </a>
+                    <span class="clearfix"></span>
+                </li>
+                <li class="list-group-item">
+                    <a href="#">
+                        <span class="name">Aula 04</span>
+                        <i class="fa fa-circle online"></i>
+                    </a>
+                    <span class="clearfix"></span>
+                </li>
+                <li class="list-group-item">
+                    <a href="#">
+                        <span class="name">Aula 05</span>
+                        <i class="fa fa-circle away"></i>
+                    </a>
+                    <span class="clearfix"></span>
+                </li>
+                <li class="list-group-item">
+                    <a href="#">
+                        <span class="name">Aula 06</span>
+                        <i class="fa fa-circle away"></i>
+                    </a>
+                    <span class="clearfix"></span>
+                </li>
+                <li class="list-group-item">
+                    <a href="#">
+                        <span class="name">Aula 07</span>
+                        <i class="fa fa-circle away"></i>
+                    </a>
+                    <span class="clearfix"></span>
+                </li>
+                <li class="list-group-item">
+                    <a href="#">
+                        <span class="name">Aula 08</span>
+                        <i class="fa fa-circle offline"></i>
+                    </a>
+                    <span class="clearfix"></span>
+                </li>
+                <li class="list-group-item">
+                    <a href="#">
+                        <span class="name">Aula 09</span>
+                        <i class="fa fa-circle offline"></i>
+                    </a>
+                    <span class="clearfix"></span>
+                </li>
+                <li class="list-group-item">
+                    <a href="#">
+                        <span class="name">Aula 10</span>
+                        <i class="fa fa-circle offline"></i>
+                    </a>
+                    <span class="clearfix"></span>
+                </li>
+            </ul>
+        </div>
+    </div>
+@endsection
