076b8d4b652e2c9539e565d4c61f7c5bb1e1725c
Marcus Vinícius Campos <marcus_vinicius_campos@hotmail.com>
2017-03-19 13:35:14 -0300

    Templating 10%

---
+++ school/resources/views/template/assets/css.blade.php
@@ -0,0 +1,6 @@
+<link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('assets/css/core.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('assets/css/components.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('assets/css/icons.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('assets/css/pages.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('assets/css/responsive.css') }}" rel="stylesheet" type="text/css" />
