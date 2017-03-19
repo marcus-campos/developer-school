2d234e4629c92274cc688a6859d4e0aaf00f4cc6
Marcus Vinícius Campos <marcus_vinicius_campos@hotmail.com>
2017-03-19 14:00:26 -0300

    Templating 30%

---
+++ resources/views/blank/index.blade.php
@@ -0,0 +1,4 @@
+@extends('vendor.template.layout')
+@extends('vendor.template.assets.css')
+@extends('vendor.template.assets.js')
+@extends('vendor.template.menu.left-menu')
--- school/resources/views/template/assets/css.blade.php
+++
@@ -1,6 +0,0 @@
-<link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
-<link href="{{ asset('assets/css/core.css') }}" rel="stylesheet" type="text/css" />
-<link href="{{ asset('assets/css/components.css') }}" rel="stylesheet" type="text/css" />
-<link href="{{ asset('assets/css/icons.css') }}" rel="stylesheet" type="text/css" />
-<link href="{{ asset('assets/css/pages.css') }}" rel="stylesheet" type="text/css" />
-<link href="{{ asset('assets/css/responsive.css') }}" rel="stylesheet" type="text/css" />
