98c2ddfe0e651ddaf29238bbbdb74095fa506bdd
Marcus Vinícius Campos <marcus_vinicius_campos@hotmail.com>
2017-03-19 23:50:54 -0300

    New feature: Series

---
+++ resources/views/series/index.blade.php
@@ -0,0 +1,93 @@
+@extends('template')
+
+@section('container')
+    <div class="row">
+        <div class="masonry-container">
+            <div class="col-sm-6 col-lg-3 col-md-4">
+                <div class="card-box">
+                    <p>
+                        <img src="assets/images/gallery/3.jpg" class="thumb-img" alt="work-thumbnail">
+                    </p>
+                    <h4 class="header-title m-t-0"><b>Your title here</b></h4>
+
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-3 col-md-4">
+                <div class="card-box">
+                    <p>
+                        <img src="assets/images/gallery/3.jpg" class="thumb-img" alt="work-thumbnail">
+                    </p>
+                    <h4 class="header-title m-t-0"><b>Your title here</b></h4>
+
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-3 col-md-4">
+                <div class="card-box">
+                    <p>
+                        <img src="assets/images/gallery/3.jpg" class="thumb-img" alt="work-thumbnail">
+                    </p>
+                    <h4 class="header-title m-t-0"><b>Your title here</b></h4>
+
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-3 col-md-4">
+                <div class="card-box">
+                    <p>
+                        <img src="assets/images/gallery/3.jpg" class="thumb-img" alt="work-thumbnail">
+                    </p>
+                    <h4 class="header-title m-t-0"><b>Your title here</b></h4>
+
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-3 col-md-4">
+                <div class="card-box">
+                    <p>
+                        <img src="assets/images/gallery/3.jpg" class="thumb-img" alt="work-thumbnail">
+                    </p>
+                    <h4 class="header-title m-t-0"><b>Your title here</b></h4>
+
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-3 col-md-4">
+                <div class="card-box">
+                    <p>
+                        <img src="assets/images/gallery/3.jpg" class="thumb-img" alt="work-thumbnail">
+                    </p>
+                    <h4 class="header-title m-t-0"><b>Your title here</b></h4>
+
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-3 col-md-4">
+                <div class="card-box">
+                    <p>
+                        <img src="assets/images/gallery/3.jpg" class="thumb-img" alt="work-thumbnail">
+                    </p>
+                    <h4 class="header-title m-t-0"><b>Your title here</b></h4>
+
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+@endsection
